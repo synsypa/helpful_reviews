e24a02ac6a015bae9a6a06ba99ce6062e4b6c86b
synsypa <changjiang23@gmail.com>
2016-06-29 15:43:14 -0400

    added logit classifer

---
+++ logit_class.py
@@ -0,0 +1,74 @@
+from __future__ import unicode_literals 
+import dill
+
+import pandas as pd
+import numpy as np
+import sklearn as sk
+
+import sklearn.metrics
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import RandomizedLogisticRegression, LogisticRegression
+
+from sklearn.cross_validation import cross_val_score
+
+# sklearn.linear_model.RandomizedLogisticRegression uses a depreciated function
+# ignore the depreciation error
+import warnings
+warnings.filterwarnings('ignore')
+
+# Load Data
+df = pd.read_pickle('parsed_df.pkl')
+
+# Construct Balanced Subset
+df['help_class'] = np.where(df['help_rate'] >= .8, 1, 0)
+
+good_df = df[df['help_class'] == 1]
+good_df = good_df.sample(n=7500, random_state=123456)
+
+bad_df = df[df['help_class'] == 0]
+bad_df = bad_df.sample(n=7500, random_state=123456)
+
+cut_df = good_df.append(bad_df)
+
+X_df = cut_df.drop('help_rate', axis = 1)
+y_df = cut_df['help_class']
+
+# Column Selection Transformer
+class ColumnTransformer(sk.base.BaseEstimator, sk.base.TransformerMixin):
+    """
+    Select Feature Columns
+    """
+    def __init__(self, cols):
+        self.cols = cols
+        pass
+
+    def fit(self, X, y):
+        return self
+
+    def transform(self, X):
+        assert(type(X) == pd.core.frame.DataFrame), 'Features must be a DataFrame'
+        try:
+            return X.loc[:,(self.cols)]
+        except (TypeError, KeyError):
+            raise TypeError('Column selection must be list of strings')
+
+# Pipeline for Randomized Logit Regression selected Logit
+features = ['length', 'dfine_pct', 'dcoarse_pct', 'ent_pct', 'quant_pct', 
+            'sent_len', 'sent_fine', 'sent_coarse', 'sent_ent',  'sent_quant']
+
+rlr_mod = Pipeline([
+                    ('select', ColumnTransformer(features)),
+                    ('rlr', RandomizedLogisticRegression(random_state=123456)),
+                    ('logit', LogisticRegression())
+                    ])
+
+# Accuracy Score
+acc = cross_val_score(rlr_mod, X_df, y_df, cv=5, scoring='accuracy').mean()
+
+# Coefficient Weights 
+weights = dict(zip(features, rlr_mod.named_steps['rlr'].scores_))
+#dill.dump(weights, open('logit_coef', 'w'), recurse=True)
+
+# Fit Model
+rlr_mod.fit(X_df, y_df)
+dill.dump(rlr_mod, open('logit_class', 'w'), recurse=True)
